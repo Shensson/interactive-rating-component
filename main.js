fcaf763ee5312ebe65dff56b5203e9b83b7914c0
shensson <daniali13ion@gmail.com>
2022-09-06 17:29:08 +0300

    visually done, need some fixing, display switch doesnt work

---
+++ main.js
@@ -0,0 +1,40 @@
+
+function youSelected1(){
+    var result = document.getElementById('result')
+
+if(document.hasFocus()){
+    result.innerHTML = "You selected 1 out of 5"
+}
+}
+
+function youSelected2(){
+    var result = document.getElementById('result')
+    
+if(document.hasFocus()){
+    result.innerHTML = "You selected 2 out of 5"
+}
+}
+
+function youSelected3(){
+    var result = document.getElementById('result')
+    
+if(document.hasFocus()){
+    result.innerHTML = "You selected 3 out of 5"
+}
+}
+
+function youSelected4(){
+    var result = document.getElementById('result')
+    
+if(document.hasFocus()){
+    result.innerHTML = "You selected 4 out of 5"
+}
+}
+
+function youSelected5(){
+    var result = document.getElementById('result')
+    
+if(document.hasFocus()){
+    result.innerHTML = "You selected 5 out of 5"
+}
+}
